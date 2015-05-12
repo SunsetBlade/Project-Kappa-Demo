18dd571e2a5f7841eb075dba14c375b264200702
Nicholas Laws <nlaws101@gmail.com>
2015-05-12 01:35:44 -0400

    testing adding empty script
    adding a bunch of empty scripts to test github

---
+++ CombatManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//iterate over list of AttackComponents, updating each one
+
+	}
+}
